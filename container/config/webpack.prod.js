dff8529f5ce709a0fe6c814f815f1df8c07d670e
satyam4p <satyamkumar343@gmail.com>
2023-10-30 17:07:18 +0530

    fixed public path for prod webpack

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -8,6 +8,9 @@ const prodFConfig = {
   mode: "production",
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: "/container/latest/" 
+    /**this path specifies to html webpack plugin in case it picking upa nay created js
+     file after build it needs to follow public path is we have custom path where files will be present after build, specially in production env */
   },
   plugins: [
     new ModuleFederationPlugin({
